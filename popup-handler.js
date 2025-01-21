a2f38402b9d311b818b55bb201eeeab19e96c42c
enryFreeday <enrico.lanciotti@freeday15.it>
2025-01-21 15:58:45 +0100

    Update popup-handler.js

--- popup-handler.js
+++ popup-handler.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const closeButton = document.querySelector('#close-popup');
-    const popup = document.querySelector('#custom-popup');
+    setTimeout(() => {
+        const closeButton = document.querySelector('#close-popup');
+        const popup = document.querySelector('#custom-popup');
 
-    // Debugging
-    console.log("Close button trovato:", closeButton);
-    console.log("Popup trovato:", popup);
+        // Debugging
+        console.log("Close button trovato:", closeButton);
+        console.log("Popup trovato:", popup);
 
-    if (closeButton && popup) {
-        closeButton.addEventListener('click', () => {
-            console.log("La X è stata cliccata");
-            popup.remove();
-            console.log('Popup chiuso con successo.');
-        });
-    } else {
-        console.error('Non è stato possibile trovare il popup o il bottone di chiusura.');
-    }
+        if (closeButton && popup) {
+            closeButton.addEventListener('click', () => {
+                console.log("La X è stata cliccata");
+                popup.remove();
+                console.log('Popup chiuso con successo.');
+            });
+        } else {
+            console.error('Non è stato possibile trovare il popup o il bottone di chiusura.');
+        }
+    }, 2000); // Timeout di 2 secondi
 });
+
 
